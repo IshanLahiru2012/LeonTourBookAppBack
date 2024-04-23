1087a04885ead97a3cd7c3c1115e8b5765559b1b
Ishan Lahiru <ishanlahiru2012@gmail.com>
2024-04-23 19:15:30 +0530

    Changed

---
+++ TourBookApp_Back/dist/middleware/validation.js
@@ -0,0 +1,16 @@
+import { body, validationResult } from "express-validator";
+const handleValidationErrors = async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+};
+export const validateUserRequest = [
+    body("name").isString().notEmpty().withMessage("Name must be a string"),
+    body("addressLine1").isString().notEmpty().withMessage("addressLine1 must be a string"),
+    body("city").isString().notEmpty().withMessage("city must be a string"),
+    body("country").isString().notEmpty().withMessage("country must be a string"),
+    handleValidationErrors
+];
+//# sourceMappingURL=validation.js.map
