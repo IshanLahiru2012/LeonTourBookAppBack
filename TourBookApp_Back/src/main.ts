1215c99c29aa03343ae92fb51dbb0acba2fe5173
Ishan Lahiru <ishanlahiru2012@gmail.com>
2024-04-23 17:51:00 +0530

    Separated back and front

---
+++ TourBookApp_Back/src/main.ts
@@ -0,0 +1,31 @@
+import express, {Request, Response} from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose"
+import userRoutes from "./routes/userRoutes.js";
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
+    .then( () => console.log("connected to database"))
+    .catch((error) => console.error("There was a problem to connect database :",error));
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+const server = app.listen(process.env.PORT || 5050,()=> {
+    console.log("app is listening at port 5050 ");
+    })
+    .on('error', (err)=>{
+        if(err.cause === 'EADDRINUSE'){
+            console.log('Port 5050 already in use. Please try a different port');
+        }else {
+            console.error(err);
+        }
+    });
+
+app.get("/",async (req:Request, res:Response)=>{
+    res.json({message:"hello nodeJs"});
+});
+
+app.use("/api/v1/user",userRoutes)
---
+++ TourBookApp_Back/src/routes/userRoutes.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import {jwtCheck, jwtParse} from "../middleware/auth.js";
+import userController from "../controllers/userController.js";
+import {validateUserRequest} from "../middleware/validation.js";
+
+const router = express.Router();
+
+router.get("/", jwtCheck, jwtParse, userController.getCurrentUser)
+router.post("/", jwtCheck, userController.createCurrentUser);
+router.put("/", jwtCheck, jwtParse,validateUserRequest, userController.updateCurrentUser)
+
+export default router;
